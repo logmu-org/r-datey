c596ec177216f2b680e0a6e9aac9f375134ea036
Tim Gordon <tim_gordon@btinternet.com>
2026-04-23 18:48:21 +0100

    Basis datey stuff works. Still to do S3 start, mid and end_day, and docs!

--- tests/testthat/test-datey.R
+++ tests/testthat/test-datey.R
@@ -1,21 +1,27 @@
 test_that("`is_leap_year` works", {
   expect_identical(datey::is_leap_year(2009.0), FALSE)
   expect_identical(datey::is_leap_year(2008L),  TRUE)
-  expect_identical(datey::is_leap_year(2000),  TRUE)
+  expect_identical(datey::is_leap_year(2000.1),  TRUE)
   expect_identical(datey::is_leap_year(1901),  FALSE)
   expect_identical(datey::is_leap_year(1900),  FALSE)
+  expect_identical(datey::is_leap_year(as_datey("2044-01-02.0")), TRUE)
+  expect_identical(datey::is_leap_year(as_datey("2047-12-29.0")), FALSE)
+  expect_identical(datey::is_leap_year(as.Date("1804-04-01.0")), TRUE)
+  expect_identical(datey::is_leap_year(as.Date("1813-08-30.0")), FALSE)
 })
 
 test_that("`is_leap_year` gives `NA` for invalid args", {
   expect_equal(datey::is_leap_year(NA), NA)
-  expect_equal(datey::is_leap_year(2000.1), NA)
   expect_equal(datey::is_leap_year("2000"), NA)
-  #expect_equal(datey::is_leap_year(as.Date("2009-08-02")), NA)
 })
 
-test_datey <- function(year, month, day, day_fraction, valid) {
-  datey <- datey::datey(year = year, month = month, day = day, day_fraction = day_fraction)
-  ymdf <- datey::as.ymdf(datey)
+test_that("a `datey` is numeric", {
+  expect_identical(base::is.numeric(datey::datey(2000, 1, 1, 0)), TRUE)
+})
+
+test_datey_round_trip <- function(year, month, day, day_fraction, valid) {
+  datey <- datey::datey(year, month, day, day_fraction)
+  ymdf <- datey::as_ymdf(datey)
   expect_identical(ymdf$year, ifelse(valid, as.integer(year), NA_integer_))
   expect_identical(ymdf$month, ifelse(valid, as.integer(month), NA_integer_))
   expect_identical(ymdf$day, ifelse(valid, as.integer(day), NA_integer_))
@@ -23,8 +29,8 @@ test_datey <- function(year, month, day, day_fraction, valid) {
 }
 
 test_valid <- function(year, month, day, day_fraction) {
-  datey <- datey::datey(year = year, month = month, day = day, day_fraction = day_fraction)
-  ymdf <- datey::as.ymdf(datey)
+  datey <- datey::datey(year, month, day, day_fraction)
+  ymdf <- datey::as_ymdf(datey)
   expect_identical(ymdf$year, as.integer(year))
   expect_identical(ymdf$month, as.integer(month))
   expect_identical(ymdf$day, as.integer(day))
@@ -32,53 +38,72 @@ test_valid <- function(year, month, day, day_fraction) {
 }
 
 test_that("`datey` round-trips from ymdf and back", {
-  test_datey(year = 1800, month = 1, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 1857, month = 1, day = 31, day_fraction = 0.25, valid = TRUE)
-  test_datey(year = 1899, month = 2, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 1904, month = 2, day = 29, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2000, month = 2, day = 29, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2004, month = 2, day = 29, day_fraction = 0, valid = TRUE)
-  test_datey(year = 1901, month = 3, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 1932, month = 3, day = 31, day_fraction = 0, valid = TRUE)
-  test_datey(year = 1945, month = 4, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 1976, month = 4, day = 30, day_fraction = 0, valid = TRUE)
-  test_datey(year = 1989, month = 5, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2010, month = 5, day = 31, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2023, month = 6, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2054, month = 6, day = 30, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2067, month = 7, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2098, month = 7, day = 31, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2101, month = 8, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2132, month = 8, day = 31, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2145, month = 9, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2176, month = 9, day = 30, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2189, month = 10, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2210, month = 10, day = 31, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2223, month = 11, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2254, month = 11, day = 30, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2267, month = 12, day = 1, day_fraction = 0, valid = TRUE)
-  test_datey(year = 2298, month = 12, day = 31, day_fraction = 0, valid = TRUE)
+  test_datey_round_trip(1800, 1, 1, 0, valid = TRUE)
+  test_datey_round_trip(1857, 1, 31, 0.25, valid = TRUE)
+  test_datey_round_trip(1899, 2, 1, 0, valid = TRUE)
+  test_datey_round_trip(1904, 2, 29, 0, valid = TRUE)
+  test_datey_round_trip(2000, 2, 29, 0, valid = TRUE)
+  test_datey_round_trip(2004, 2, 29, 0, valid = TRUE)
+  test_datey_round_trip(1901, 3, 1, 0, valid = TRUE)
+  test_datey_round_trip(1932, 3, 31, 0, valid = TRUE)
+  test_datey_round_trip(1945, 4, 1, 0, valid = TRUE)
+  test_datey_round_trip(1976, 4, 30, 0, valid = TRUE)
+  test_datey_round_trip(1989, 5, 1, 0, valid = TRUE)
+  test_datey_round_trip(2010, 5, 31, 0, valid = TRUE)
+  test_datey_round_trip(2023, 6, 1, 0, valid = TRUE)
+  test_datey_round_trip(2054, 6, 30, 0, valid = TRUE)
+  test_datey_round_trip(2067, 7, 1, 0, valid = TRUE)
+  test_datey_round_trip(2098, 7, 31, 0, valid = TRUE)
+  test_datey_round_trip(2101, 8, 1, 0, valid = TRUE)
+  test_datey_round_trip(2132, 8, 31, 0, valid = TRUE)
+  test_datey_round_trip(2145, 9, 1, 0, valid = TRUE)
+  test_datey_round_trip(2176, 9, 30, 0, valid = TRUE)
+  test_datey_round_trip(2189, 10, 1, 0, valid = TRUE)
+  test_datey_round_trip(2210, 10, 31, 0, valid = TRUE)
+  test_datey_round_trip(2223, 11, 1, 0, valid = TRUE)
+  test_datey_round_trip(2254, 11, 30, 0, valid = TRUE)
+  test_datey_round_trip(2267, 12, 1, 0, valid = TRUE)
+  test_datey_round_trip(2298, 12, 31, 0, valid = TRUE)
 
-  test_datey(year = 2001, month = 2, day = 29, day_fraction = 0, valid = FALSE)
-  test_datey(year = 2000, month = 3, day = 15, day_fraction = 1, valid = FALSE)
+  test_datey_round_trip(2001, 2, 29, 0, valid = FALSE)
+  test_datey_round_trip(2000, 3, 0, 0, valid = FALSE)
 })
 
 test_that("`datey` works with vector and scalar `day_fraction`s", {
 
-  year = c(1960, 2001, 2099);
-  month = c(11, 3, 7);
-  day = c(3, 31, 15);
-  df_scalar = 0.25;
-  df_vector = c(0.25, 0.25, 0.25);
+  year <- c(1960, 2001, 2099);
+  month <- c(11, 3, 7);
+  day <- c(3, 31, 15);
+  df_scalar <- 0.25;
+  df_vector <- c(0.25, 0.25, 0.25);
 
-  datey_scalar <- datey::datey(year = year, month = month, day = day,
-    day_fraction = df_scalar)
+  datey_scalar <- datey::datey(year, month, day,
+                               df_scalar)
 
-  datey_vector <- datey::datey(year = year, month = month, day = day,
-    day_fraction = df_vector)
+  datey_vector <- datey::datey(year, month, day,
+                               df_vector)
 
 
   expect_identical(datey_vector, datey_scalar)
 })
 
+test_that("`datey` matches `Date`", {
+  expect_identical(as_datey("1000-01-01.0"), as_datey(as.Date("1000-01-01"), 0))
+  expect_identical(as_datey("2999-12-31.0"), as_datey(as.Date("2999-12-31"), 0))
+  expect_identical(as_datey("0999-12-31.5"), as_datey(as.Date("0999-12-31"), 0))
+  expect_identical(as_datey("3000-01-01.0"), as_datey(as.Date("3000-01-01"), 0))
+})
 
+test_that("`datey` matches `POSIXct`", {
+  expect_identical(as_datey("1000-01-01.0"), as_datey(as.POSIXct("1000-01-01")))
+  expect_identical(as_datey("2999-12-31.5"), as_datey(as.POSIXct("2999-12-31 12:00")))
+  expect_identical(as_datey("0999-12-31.5"), as_datey(as.POSIXct("0999-12-31 12:00")))
+  expect_identical(as_datey("3000-01-01.0"), as_datey(as.POSIXct("3000-01-01")))
+})
+
+test_that("`datey` matches `POSIXlt`", {
+  expect_identical(as_datey("1000-01-01.0"), as_datey(as.POSIXlt("1000-01-01")))
+  expect_identical(as_datey("2999-12-31.5"), as_datey(as.POSIXlt("2999-12-31 12:00")))
+  expect_identical(as_datey("0999-12-31.5"), as_datey(as.POSIXlt("0999-12-31 12:00")))
+  expect_identical(as_datey("3000-01-01.0"), as_datey(as.POSIXlt("3000-01-01")))
+})
