bf84b93a214d1d4965b75514b65a122d9e3c2f87
Tim Gordon <tim_gordon@btinternet.com>
2026-05-28 18:13:37 +0100

    Added year, month etc to datey. Tidy up docs.

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+# Date and duration arithmetic on an annual grid for R
+#
+# This file is licensed to you under the MIT License.
+#
+# Copyright (c) Tim Gordon
+
+if (getRversion() >= "2.15.1")  utils::globalVariables("pillar_shaft_character")
